f4cc90b0a256873bbb5c7e035ba72ae56ca443e7
Tristin Smith <tsmithassoc@gmail.com>
2024-06-06 19:04:00 -0400

    Initial Commit

---
+++ RockPaperScissorsWithATwist/RockPaperScissorsWithATwistApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsWithATwistApp.swift
+//  RockPaperScissorsWithATwist
+//
+//  Created by Tristin Smith on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsWithATwistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
